be453d2e986dbd1b268284cd52bf224c23cd3cf7
Vsevolod Glumov <seva17@gmail.com>
2019-08-13 18:48:50 -0700

    Add some basic tests

--- urlshort/urlshort_web/tests.py
+++ urlshort/urlshort_web/tests.py
@@ -1,3 +1,62 @@
+import json
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+# Inherit from this in your test cases
+class GraphQLTestCase(TestCase):
+
+    def setUp(self):
+        self._client = Client()
+
+    def query(self, query: str, op_name: str = None, input: dict = None):
+        '''
+        Args:
+            query (string) - GraphQL query to run
+            op_name (string) - If the query is a mutation or named query, you must
+                               supply the op_name.  For annon queries ("{ ... }"),
+                               should be None (default).
+            input (dict) - If provided, the $input variable in GraphQL will be set
+                           to this value
+
+        Returns:
+            dict, response from graphql endpoint.  The response has the "data" key.
+                  It will have the "error" key if any error happened.
+        '''
+        body = {'query': query}
+        if op_name:
+            body['operation_name'] = op_name
+        if input:
+            body['variables'] = {'input': input}
+
+        resp = self._client.post('/graphql', json.dumps(body),
+                                 content_type='application/json')
+        jresp = json.loads(resp.content.decode())
+        return jresp
+
+    def assertResponseNoErrors(self, resp: dict, expected: dict):
+        '''
+        Assert that the resp (as retuened from query) has the data from
+        expected
+        '''
+        self.assertNotIn('errors', resp, 'Response had errors')
+        self.assertEqual(resp['data'], expected, 'Response has correct data')
+
+    def test_creation_and_reuse(self):
+        resp = self.query('''query {
+            shortUrl(url: "https://www.google.com/")
+          }''')
+        self.assertIn('shortUrl', resp['data'])
+        key = resp['data']['shortUrl']
+        resp = self.query('''query {
+            shortUrl(url: "https://www.google.com/")
+          }''')
+        self.assertEqual(resp['data']['shortUrl'], key)
+    
+    def test_retrieve_full_url_by_short(self):
+        resp = self.query('''query {
+            shortUrl(url: "https://www.google.com/")
+          }''')
+        key = resp['data']['shortUrl']
+        resp = self.query('query {url(shortUrl: "%s")}' % key)
+        self.assertIn('url', resp['data'])
+    
